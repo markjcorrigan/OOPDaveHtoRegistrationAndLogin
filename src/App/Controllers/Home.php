41a57236df680a53c5f8f42e1ebf850bbad00776
Mark Corrigan <markjc@mweb.co.za>
2025-03-24 21:17:18 +0200

    my first installation of daves new code

---
+++ src/App/Controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\Controller;
+use Framework\Response;
+
+class Home extends Controller
+{
+    public function index(): Response
+    {
+        echo $this->viewer->render("shared/header.php", [
+            "title" => "Home"
+        ]);
+
+        echo $this->viewer->render("Home/index.php");
+    }
+}
