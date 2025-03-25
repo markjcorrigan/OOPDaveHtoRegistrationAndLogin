459914c39d81b09b1f6f4e5330b0a019ca9d107a
Mark Corrigan <markjc@mweb.co.za>
2025-03-25 12:35:11 +0200

    I fixed the Home controller problem

--- src/App/Controllers/Home.php
+++ src/App/Controllers/Home.php
@@ -11,10 +11,13 @@ class Home extends Controller
 {
     public function index(): Response
     {
-        echo $this->viewer->render("shared/header.php", [
+//        echo $this->viewer->render("shared/header.php", [
+//            "title" => "Home"
+//        ]);
+//
+//        echo $this->viewer->render("Home/index.php");
+        return $this->view("Home/index.mvc.php", [
             "title" => "Home"
         ]);
-
-        echo $this->viewer->render("Home/index.php");
     }
 }
---
+++ views/Home/index.mvc.php
@@ -0,0 +1,11 @@
+{% extends "base.mvc.php" %}
+
+{% block title %}Home{% endblock %}
+
+{% block body %}
+
+<h1>Home Page</h1>
+
+<a href="/products/index">Click here for the products section</a>
+
+{% endblock %}
