072d6c7f55926743d729adc5f540278233d19104
Mark Corrigan <markjc@mweb.co.za>
2025-04-07 20:52:45 +0200

    Added descriptions for the variables in Login\success.mvc.php

--- views/Login/success.mvc.php
+++ views/Login/success.mvc.php
@@ -6,13 +6,13 @@
 
 <h1>New Sign In</h1>
 
+<p>The below variable names before : are between {{  }} in the templating engine </p>
+<p> userObj->id : {{ userObj->id }}</p>
+<p> userObj->id : {{ userObj->name }}</p>
+<p> userObj->id : {{ userObj->email }}</p>
 
-<p>{{ userObj->id }}</p>
-<p>{{ userObj->name }}</p>
-<p>{{ userObj->email }}</p>
-
-<p>{{ email }}</p>
-<p>{{ password }}</p>
+<p> email : {{ email }}</p>
+<p> password : {{ password }}</p>
 <p>You have successfully signed in</p>
 <a href="/">Take me home</a><br>
 
