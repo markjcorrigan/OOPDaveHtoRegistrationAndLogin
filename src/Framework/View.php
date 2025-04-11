1a4399c72b245d4311b14d156ef7f58413840717
Mark Corrigan <markjc@mweb.co.za>
2025-04-11 19:42:30 +0200

    using Home/index as twig I have managed to incorporate twig into the framework

---
+++ src/Framework/View.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Framework;
+
+/**
+ * View
+ *
+ * PHP version 7.0
+ */
+class View
+{
+
+    /**
+     * Render a view file
+     *
+     * @param string $view  The view file
+     * @param array $args  Associative array of data to display in the view (optional)
+     *
+     * @return void
+     */
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+
+        $file = dirname(__DIR__) . "'/../views'/$view";  // relative to Framework directory
+
+        if (is_readable($file)) {
+            require $file;
+        } else {
+            throw new \Exception("$file not found");
+        }
+    }
+
+    /**
+     * Render a view template using Twig
+     *
+     * @param string $template  The template file
+     * @param array $args  Associative array of data to display in the view (optional)
+     *
+     * @return void
+     */
+    public static function renderTemplate($template, $args = [])
+    {
+        echo static::getTemplate($template, $args);
+    }
+
+    /**
+     * Get the contents of a view template using Twig
+     *
+     * @param string $template  The template file
+     * @param array $args  Associative array of data to display in the view (optional)
+     *
+     * @return string
+     */
+    public static function getTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/../views');
+			$twig = new \Twig\Environment($loader);
+    //        $twig->addGlobal('current_user', \App\Auth::getUser());
+    //        $twig->addGlobal('flash_messages', \Framework\Flash::getMessages());
+        }
+
+        return $twig->render($template, $args);
+    }
+}
